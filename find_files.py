dada2030efa035fb74f9682fc34da540880da5dc
KirillNN <wmforum@bk.ru>
2021-04-27 16:58:54 +0300

    Summary

---
+++ find_files.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+main_dir = r'c:\Users\Kirill\HowToMakeAVideoGame'
+
+os.chdir(main_dir)
+ar_name = []
+count = 0
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        # pass
+        f_name = os.path.join(root, name)
+        size = os.path.getsize(f_name)
+        if size > 1024 * 1024 * 100:  # если больше 100 МБ
+            print(f_name, end=' ')
+            print(size, 'bytes')
+            ar_name.append(f_name)
+        count += 1
+    # for name in dirs:
+    #     print(os.path.join(root, name))
+
+print(count)
+# print(ar_name)
+# os.chdir(dist_dir)  # Переходим в директорию для копирования
+
+dist_dir = r'f:\tmp2'
+for item in ar_name:  # item = '.\Library\il2cpp_cache\linkresult_B5B242D3FD8025D63DCF2ECCC55A7FF5\build.bc'
+    # print(item.rfind('\\'))
+    # print(item[2:item.rfind('\\')])  # item` = 'Library\il2cpp_cache\linkresult_B5B242D3FD8025D63DCF2ECCC55A7FF5'
+    try:
+        os.makedirs(dist_dir + '\\' + item[2:item.rfind('\\')])  # создаем директорию "приемник"
+    except FileExistsError:
+        pass
+    shutil.copy2(main_dir + item[1:], dist_dir + '\\' + item[2:item.rfind('\\')])  # копируем файл в "приемник"
+    os.remove(main_dir + item[1:])  # удаляем исходный файл - получем функцию перемещения
